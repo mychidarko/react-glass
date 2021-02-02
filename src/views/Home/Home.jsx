749c4d2c5ba77d067b5ff8abe3a52b19a846b287
mychi darko <mickdd22@gmail.com>
2021-02-02 16:10:01 +0000

    :art: made home screen more interesting

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -8,18 +8,20 @@ export default function Home() {
 
   setTimeout(() => {
     setInitial("hobies");
-  }, 3000);
+  }, 6000);
 
   return (
     <div className="flex flex:center-all" style={{ height: "100vh" }}>
       <div className="px:_3 py:_5" style={{ width: "50%" }}>
         <h1 className="mb:_2">Welcome to React Glass!</h1>
         <p>
-          {initial}
           React Glass is a simple react js boilerplate which focuses on providing a simple and pain-free
           developer experience. React Glass uses items like named routes, simple state management instead of
           overblown libraries like redux and unfriendly interfaces like react-router-dom.
           With React Glass, the simpler, the better.
+        </p>
+        <p className="mt:_2">
+          This will change to hobbies in 5 seconds 👉 {initial}
         </p>
       </div>
     </div>
